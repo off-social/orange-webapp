82540109f46e0dda3a0186bd542acd90332eebeb
Mansi Jethloja <mansi.beyondblueprints@gmail.com>
2026-05-25 16:41:39 +0530

    added sections to homepage inside components folder

---
+++ components/home/tab-section.tsx
@@ -0,0 +1,10 @@
+
+export default function TabSection() {
+    return (
+        <div className="max-w-7xl mx-auto px-6 py-16">
+            <h1 className="text-4xl font-bold">
+                Tab Section
+            </h1>
+        </div>
+    );
+}
